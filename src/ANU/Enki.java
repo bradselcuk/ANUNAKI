6b62d99a91eedfcc8ffbc46202d140e96f470d6c
bradselcuk <bradselcuk@gmail.com>
2021-07-22 09:41:44 -0500

    ba

--- src/ANU/Enki.java
+++ src/ANU/Enki.java
@@ -5,6 +5,7 @@ public class Enki {
     public static void main(String[] args) {
         System.out.println("ENKI NE YAPTI");
         System.out.println("ENU ENKININ KARDESI");
+        System.out.println("KRAL ANU IKISININ BABASI");
     }
 
 }
