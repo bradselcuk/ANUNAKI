b5ea5a5b4a7d7246971118013037b290723f0eec
bradselcuk <bradselcuk@gmail.com>
2021-07-22 09:39:11 -0500

    en

--- src/ANU/Enki.java
+++ src/ANU/Enki.java
@@ -1,7 +1,10 @@
 package ANU;
 
 public class Enki {
+
     public static void main(String[] args) {
         System.out.println("ENKI NE YAPTI");
+        System.out.println("ENU ENKININ KARDESI");
     }
+
 }
